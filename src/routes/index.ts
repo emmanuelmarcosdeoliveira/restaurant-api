e68fa038ae212842254cae157e2914d4b6e83e05
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-06-05 12:50:06 -0300

    chore: created the controller to handle the table section table

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,9 @@ import { Router } from "express";
 
 import { productsRoutes } from "./products-routes.js";
 import { tablesRoutes } from "./tables-routes.js";
+import { tablesSessionsRoutes } from "./tables-sessions-routes";
 const routes = Router();
 routes.use("/products", productsRoutes);
 routes.use("/tables", tablesRoutes);
-
+routes.use("/tables-sessions", tablesSessionsRoutes);
 export { routes };
